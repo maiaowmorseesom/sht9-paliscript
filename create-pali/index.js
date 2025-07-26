c23dbc7be47aacb2195fb8ea7e7a9eaf345582fb
IkaWaAyuMu <pungpond4450@gmail.com>
2025-07-26 17:49:08 +0700

    chore: boilterplate to r/w file

--- create-pali/index.js
+++ create-pali/index.js
@@ -1,3 +1,26 @@
-module.exports = (args) => {
-    console.log('Running create-pali with args:', args);
+const fs = require("fs/promises");
+const path = require("path");
+
+module.exports = async (args) => {
+  await Promise.all(
+    args.map(async (file) => {
+      try {
+        const data = await fs.readFile(file, "utf-8");
+
+        // REPLACE BELOW WITH PROCESSING LOGIC
+        const processedData = data.toUpperCase();
+        // END
+
+        const { dir, name } = path.parse(file);
+        const newFileName = `${name}.paliscript`;
+        const newFilePath = path.join(dir, newFileName);
+
+        await fs.writeFile(newFilePath, processedData);
+
+        console.log(`✅ Processed file created at: ${newFilePath}`);
+      } catch (err) {
+        console.error("❌ Error processing ", file, " : ", err.message);
+      }
+    })
+  );
 };
