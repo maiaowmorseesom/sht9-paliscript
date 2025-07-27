f16858e10567531a16f67ee03f3180d16154913c
TUN | Thanyanit Jongjitragan <46578644+Thanyanit-J@users.noreply.github.com>
2025-07-27 11:49:00 +0700

    refactor: consolidate path import

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,6 @@
 import http from "http";
 import fs from "fs";
-import path from "path";
-import { dirname } from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import open from "open";
 
